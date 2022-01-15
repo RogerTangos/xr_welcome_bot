f806639d5e7bf10c9a1ac54d3cf3e2ba01ed7284
Bart Sopers <bart@sopers.nl>
2022-01-15 11:57:33 +0000

    Add simple bash script for extracting translations from sources

---
+++ extract_translations.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# set working directory to script path
+cd "$(dirname "$0")"
+
+# collect all python files and store to a temporary file
+tmp_file=/tmp/xr_welcome_bot_py_files
+find . -name "*.py" > $tmp_file
+
+# do the actual extraction
+xgettext --keyword=translate:1 --from-code=UTF-8 --output=locale/base.pot --files-from=$tmp_file
+
+# remove temporary file
+rm $tmp_file
